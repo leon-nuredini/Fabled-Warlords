5e78366fffdfb4a55a4c0c63622d7bbde2f052af
Leon <33429205+Hir-o@users.noreply.github.com>
2024-07-17 15:26:10 +0200

    Functionalised the stun status effect for the storm elemental unit

--- Assets/Code/Scripts/Unit/UnitTypes/Primordials/StormElemental.cs
+++ Assets/Code/Scripts/Unit/UnitTypes/Primordials/StormElemental.cs
@@ -1,4 +1,24 @@
 public class StormElemental : LUnit
 {
+    private ThunderStrikeSkill _thunderStrikeSkill;
     
+    #region Properties
+
+    public ThunderStrikeSkill ThunderStrikeSkill => _thunderStrikeSkill;
+
+    #endregion
+    
+    public override void InitProperties()
+    {
+        base.InitProperties();
+        _thunderStrikeSkill = GetComponent<ThunderStrikeSkill>();
+    }
+    
+    protected override void ApplyDebuffsToEnemy(LUnit enemyUnit, bool isEnemyTurn = false)
+    {
+        if (ThunderStrikeSkill == null) return;
+        float randomChance = UnityEngine.Random.Range(0f, 100f);
+        if (randomChance < ThunderStrikeSkill.ProcChance) return;
+        enemyUnit.StatusEffectsController.ApplyStatusEffect<Stun>(_thunderStrikeSkill.DurationInTurns);
+    }
 }
