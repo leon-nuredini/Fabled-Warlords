cb57f7951ee8fca0a4ea52addd4d8a31a516d50f
Leon <33429205+Hir-o@users.noreply.github.com>
2024-07-17 15:36:54 +0200

    Applied the Poison hex skill status effect for the Druid unit

--- Assets/Code/Scripts/Unit/UnitTypes/Primordials/Druid.cs
+++ Assets/Code/Scripts/Unit/UnitTypes/Primordials/Druid.cs
@@ -1,4 +1,24 @@
-public class Druid : Monk
+public class Druid : LUnit
 {
+    private PoisonHexSkill _poisonHexSkill;
     
+    #region Properties
+
+    public PoisonHexSkill PoisonHexSkill => _poisonHexSkill;
+    
+    #endregion
+    
+    public override void InitProperties()
+    {
+        base.InitProperties();
+        _poisonHexSkill = GetComponent<PoisonHexSkill>();
+    }
+    
+    protected override void ApplyDebuffsToEnemy(LUnit enemyUnit, bool isEnemyTurn = false)
+    {
+        if (PoisonHexSkill == null) return;
+        int extraTurn = isEnemyTurn ? 1 : 0;
+        enemyUnit.StatusEffectsController.ApplyStatusEffect<Weaken>(PoisonHexSkill.DurationInTurns + extraTurn);
+        enemyUnit.StatusEffectsController.ApplyStatusEffect<Poison>(PoisonHexSkill.DurationInTurns + extraTurn);
+    }
 }
