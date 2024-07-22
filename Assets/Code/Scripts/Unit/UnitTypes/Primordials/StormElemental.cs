9746201da040da9ea25cadf26cb218e353633430
Leon Nuredini <soetsutakiwasa@hotmail.com>
2024-07-22 19:36:50 +0200

    Added thunder strike visual effect for the storm elemental

--- Assets/Code/Scripts/Unit/UnitTypes/Primordials/StormElemental.cs
+++ Assets/Code/Scripts/Unit/UnitTypes/Primordials/StormElemental.cs
@@ -32,6 +32,7 @@ public class StormElemental : LUnit, IMage
         if (enemyUnit.HitPoints <= 0) return;
         float randomChance = Random.Range(0f, 100f);
         if (randomChance > ThunderStrikeSkill.ProcChance) return;
+        if (ThunderStrikeSkill is ISpawnableEffect spawnableEffect) spawnableEffect.SpawnEffect(enemyUnit.transform);
         enemyUnit.StatusEffectsController.ApplyStatusEffect<Stun>(ThunderStrikeSkill.DurationInTurns);
     }
 }
