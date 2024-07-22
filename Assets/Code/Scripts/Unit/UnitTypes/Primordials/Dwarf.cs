252a593997fbd4ede4a1c3cfade69bbaf7004ee1
Leon Nuredini <soetsutakiwasa@hotmail.com>
2024-07-22 19:23:55 +0200

    Bugfix: Ston will ability for the dwarf unit not being applied

--- Assets/Code/Scripts/Unit/UnitTypes/Primordials/Dwarf.cs
+++ Assets/Code/Scripts/Unit/UnitTypes/Primordials/Dwarf.cs
@@ -5,10 +5,36 @@ public class Dwarf : LUnit, ISwordInfantry
 {
     protected override int Defend(Unit other, int damage)
     {
-        float newDamage = damage;
+        Agressor = other;
+        float defenceFactor = CalculateDefense();
+        float defenceAmount = damage * defenceFactor;
+        float newDamage = damage - defenceAmount;
+
         if (other is LUnit lUnit && lUnit.UnitClassCounter != null)
             newDamage *= lUnit.UnitClassCounter.VSSwordInfantryCounter;
 
-        return base.Defend(other, Mathf.RoundToInt(newDamage));
+        if (StatusEffectsController.IsStatusApplied<Weaken>())
+        {
+            float weakenedFactor = StatusEffectsController.GetStatus<Weaken>().weakenFactor;
+            newDamage = Mathf.RoundToInt(newDamage * weakenedFactor);
+        }
+
+        bool isRetalationResilenceActive = TryUseRetaliationResilence();
+        if (isRetalationResilenceActive) newDamage /= 2;
+        if (newDamage <= 0) newDamage = 1;
+        TempDamageReceived = Mathf.RoundToInt(newDamage);
+        return TempDamageReceived;
+    }
+
+    protected override float CalculateDefense()
+    {
+        float defenceAmount = 0;
+        for (int i = 0; i < DefendSkillArray.Length; i++)
+        {
+            if (DefendSkillArray[i] is StoneWillSkill)
+                defenceAmount = DefendSkillArray[i].GetDefenceAmount();
+        }
+
+        return defenceAmount;
     }
 }
