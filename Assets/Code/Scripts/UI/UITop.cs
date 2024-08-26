6b9c20306a77ee92a4b3bc1d7e3f5fb2928f3117
Leon <33429205+Hir-o@users.noreply.github.com>
2024-08-26 15:45:49 +0200

    Made it possible to open the recruitment panel when clicking on a barrack

--- Assets/Code/Scripts/UI/UITop.cs
+++ Assets/Code/Scripts/UI/UITop.cs
@@ -54,12 +54,14 @@ public class UITop : MonoBehaviour
 
     private void OnEnable()
     {
+        Barrack.OnAnyBarrackClicked += OpenRecruitmentPanelFromBarrack;
         if (CellGrid.Instance == null) return;
         CellGrid.Instance.TurnStarted += EnableEndTurnButton;
     }
 
     private void OnDisable()
     {
+        Barrack.OnAnyBarrackClicked -= OpenRecruitmentPanelFromBarrack;
         if (CellGrid.Instance == null) return;
         CellGrid.Instance.TurnStarted -= EnableEndTurnButton;
     }
@@ -97,6 +99,11 @@ public class UITop : MonoBehaviour
 
     private void OpenSettingsPanel() => OnAnyMenuButtonClicked?.Invoke();
     private void OpenRecruitmentPanel() => OnAnyRecruitButtonClicked?.Invoke();
+    private void OpenRecruitmentPanelFromBarrack(Barrack barrack) 
+    {
+        if (barrack.PlayerNumber == CellGrid.Instance.CurrentPlayerNumber)
+        OnAnyRecruitButtonClicked?.Invoke();
+    }
 
     private void EndTurn()
     {
